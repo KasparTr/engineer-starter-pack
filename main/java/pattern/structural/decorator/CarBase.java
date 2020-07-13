57e4e41efc66e52cd2c8d2fd6cd6b2a727f6795e
Kaspar <kaspartr@gmail.com>
2020-07-13 08:53:24 +0300

    Updated decorator example

--- main/java/pattern/structural/decorator/CarBase.java
+++ main/java/pattern/structural/decorator/CarBase.java
@@ -2,12 +2,13 @@ package pattern.structural.decorator;
 
 /**
  * Component Implementation – The basic implementation of the component interface. We can have BasicCar class as our component implementation.
+ * Base is required by all cars, no matter what type of car we are dealing with. This class represents the chassy, wireing, etc...
  */
-public class BasicCar implements Car {
+public class CarBase implements Car {
 
     @Override
     public void assemble() {
-        System.out.print("Assembling a Basic Car.");
+        System.out.print("Assembling the base of a Car.");
     }
 
 }
--- main/java/pattern/structural/decorator/DecoratorRunner.java
+++ main/java/pattern/structural/decorator/DecoratorRunner.java
@@ -2,11 +2,11 @@ package pattern.structural.decorator;
 
 public class DecoratorRunner {
     public static void main(String[] args) {
-        Car sportsCar = new SportsCar(new BasicCar());  // Just a sports car
+        Car sportsCar = new SportsCar(new CarBase());  // Just a sports car
         sportsCar.assemble();
         System.out.println("\n*****");
 
-        Car sportsLuxuryCar = new SportsCar(new LuxuryCar(new BasicCar())); // A sports acr, but also has Luxury carfeatures.
+        Car sportsLuxuryCar = new SportsCar(new LuxuryCar(new CarBase())); // A sports car with luxury features.
         sportsLuxuryCar.assemble();
     }
 }
