28fe00b5d6227bd8707560513f80714d67d65bdd
Kaspar Triebstok <kaspartr@gmail.com>
2020-07-13 08:37:32 +0300

    Update Triangle.java

--- main/java/pattern/structural/bridge/Triangle.java
+++ main/java/pattern/structural/bridge/Triangle.java
@@ -1,6 +1,7 @@
 package pattern.structural.bridge;
 
 public class Triangle extends Shape{
+    // Notice how Triangle doesn't HAVE-A Color anymore but its moved to the absrtact Shape class.
 
     public Triangle(Color c) {
         super(c);
