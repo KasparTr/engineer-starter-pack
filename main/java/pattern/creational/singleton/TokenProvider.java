d458c41829d6016a11811f2bed71dc427e0baeaf
Kaspar Triebstok <kaspartr@gmail.com>
2020-07-13 08:25:15 +0300

    Update TokenProvider.java

--- main/java/pattern/creational/singleton/TokenProvider.java
+++ main/java/pattern/creational/singleton/TokenProvider.java
@@ -2,6 +2,12 @@ package pattern.creational.singleton;
 
 public class TokenProvider {
 
+    private final String asciiUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private final String asciiLowerCase = asciiUpperCase.toLowerCase();
+    private final String digits = "1234567890";
+    private final String asciiChars = asciiUpperCase + asciiLowerCase + digits;
+ 
+
     private static TokenProvider instance;
     // Another option is to create during build time and return it in getInstance method() without null check.
     //private static TokenProvider instance = new TokenProvider();
@@ -16,7 +22,19 @@ public class TokenProvider {
         return instance;
     }
 
-    public void getToken(){
+    public String generateToken(){
         System.out.println("Here is your single token");
+        return generateRandomString(20, asciiChars)
+    }
+    
+    private static String generateRandomString(int length, String seedChars) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        Random rand = new Random();
+        while (i < length) {
+            sb.append(seedChars.charAt(rand.nextInt(seedChars.length())));
+            i++;
+        }
+        return sb.toString();
     }
 }
